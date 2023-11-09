168920b5488b0982c29a7c83d9154686904e1561
ikanx101 <ikanx101@gmail.com>
2023-11-09 13:03:07 +0700

    update

---
+++ Versi Baru/Data Raw/converte.R
@@ -0,0 +1,19 @@
+# ==============================================================================
+#
+# CONVERTER APPSHEET
+# Versi Neo 2023
+# by: ikanx101.com
+# last update: 9 November
+#
+# latar belakang
+# [Tuesday 8:34 AM] Chandra Bimantara
+# Mas, saat ini Appsheet akan di develop lagi utk menjadi tools mds motoris nasional
+# Jadi, ada beberapa penyesuaian mas pd struktur datanya, utk mengakomodir kebutuhan di area2 lain juga agar bisa serentak digunakan nasional
+# nah, seperti biasa mas saya ada kebutuhan untuk membuat converternya, agar data dapat di convert dan di sambungkan ke tabi nantinya
+# saya bisa minta bantuannya lagi kah mas untuk pembuatan converternya? kali ini satu data, akan di convert menjadi 2 data mas. ada data penjualan dan data AV item di operator2
+#
+# ==============================================================================
+
+
+# ==============================================================================
+# libraries yang terlibat
